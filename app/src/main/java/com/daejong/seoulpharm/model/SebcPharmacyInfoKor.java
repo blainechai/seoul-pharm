f50d9842ecfc22370cc32051b9902f081d47ed04
Seungmok Chai <blainechai@gmail.com>
2016-10-15 16:42:09 +0900

    merged 2

--- app/src/main/java/com/daejong/seoulpharm/model/SebcPharmacyInfoKor.java
+++ app/src/main/java/com/daejong/seoulpharm/model/SebcPharmacyInfoKor.java
@@ -10,17 +10,10 @@ import java.util.List;
 public class SebcPharmacyInfoKor {  // response type model
 
     @SerializedName("list_total_count")
-<<<<<<< HEAD:app/src/main/java/com/daejong/seoulpharm/model/SebcPharmacyInfoKor.java
-    public int list_total_count;
-
-    @SerializedName("RESULT")
-    public Result RESULT;
-=======
     private int listTotalCount;
     @SerializedName("RESULT")
     private Result result;
     private List<PharmItem> row;
->>>>>>> 180b046b3df8c82c20784fc8529323acdd7c618c:app/src/main/java/com/example/hyunwoo/samplenavermap/model/SebcPharmacyInfoKor.java
 
     // constructor
     public SebcPharmacyInfoKor (int listTotalCount, Result result, List<PharmItem> row) {
