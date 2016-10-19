40295d91f37cf2e60e7ad8fadadbb8bbbebc5df5
Seungmok Chai <blainechai@gmail.com>
2016-10-19 23:47:24 +0900

    navigation drawer merged

--- app/src/main/java/com/daejong/seoulpharm/MainActivity.java
+++ app/src/main/java/com/daejong/seoulpharm/MainActivity.java
@@ -1,31 +1,44 @@
 package com.daejong.seoulpharm;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.daejong.seoulpharm.db.DBHelper;
-import com.daejong.seoulpharm.mapviewer.NMapViewer;
 import com.daejong.seoulpharm.model.PharmItem;
 import com.daejong.seoulpharm.model.ResponseResult;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Toolbar toolbar;
     TextView textView;
     DBHelper db;
     Button mapBtn;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
         db = new DBHelper(MainActivity.this);
         textView = (TextView) findViewById(R.id.resultView);
+
+        setSupportActionBar(toolbar);
+        setDrawerToggle();
+
         mapBtn = (Button) findViewById(R.id.map_btn);
 
         mapBtn.setOnClickListener(this);
@@ -52,8 +65,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.map_btn:
-                startActivity(new Intent(MainActivity.this, NMapViewer.class));
                 break;
         }
+    }
+
+    private void setDrawerToggle() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+//        drawerLayout.setDrawerListener(mDrawerToggle);
+        Log.d("hi", "" + mDrawerToggle.isDrawerIndicatorEnabled());
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 }
