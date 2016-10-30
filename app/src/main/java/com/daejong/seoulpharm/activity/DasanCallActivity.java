1a45e56aa98a3894f9a243b22cf44d394cb96c15
Hyunwoo <cho7673@gmail.com>
2016-10-31 08:54:53 +0900

    before merge

--- app/src/main/java/com/daejong/seoulpharm/activity/DasanCallActivity.java
+++ app/src/main/java/com/daejong/seoulpharm/activity/DasanCallActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.daejong.seoulpharm.R;
+import com.daejong.seoulpharm.util.LanguageSelector;
 import com.daejong.seoulpharm.widget.NotoTextView;
 
 public class DasanCallActivity extends AppCompatActivity {
@@ -36,13 +38,45 @@ public class DasanCallActivity extends AppCompatActivity {
         callBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String phoneNum = "120";
-                Uri uri = Uri.parse("tel:" + phoneNum);
-                Intent intent = new Intent(Intent.ACTION_CALL, uri);
-                startActivity(intent);
+                try {
+                    String phoneNum = "120";
+                    Uri uri = Uri.parse("tel:" + phoneNum);
+                    Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                    startActivity(intent);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        int currentLanguage = LanguageSelector.getInstance().getCurrentLanguage();
+        initViewsLanguageText(currentLanguage);
 
+    }
+
+
+
+    private void initViewsLanguageText(int currentLanguage) {
+        switch (currentLanguage) {
+            case R.drawable.btn_kor :
+                textPlzTouchIconView.setText(R.string.dasan_kor_plz_touch_icon);
+                textNeedHelpView.setText(R.string.dasan_kor_need_help);
+                textDescriptionView.setText(R.string.dasan_kor_description);
+                textAvailableTime.setText(R.string.dasan_kor_available_time);
+                break;
+            case R.drawable.btn_eng :
+                textPlzTouchIconView.setText(R.string.dasan_eng_plz_touch_icon);
+                textNeedHelpView.setText(R.string.dasan_eng_need_help);
+                textDescriptionView.setText(R.string.dasan_eng_description);
+                textAvailableTime.setText(R.string.dasan_eng_available_time);
+                break;
+            case R.drawable.btn_china :
+                textPlzTouchIconView.setText(R.string.dasan_chi_plz_touch_icon);
+                textNeedHelpView.setText(R.string.dasan_chi_need_help);
+                textDescriptionView.setText(R.string.dasan_chi_description);
+                textAvailableTime.setText(R.string.dasan_chi_available_time);
+                break;
+
+        }
     }
 }
